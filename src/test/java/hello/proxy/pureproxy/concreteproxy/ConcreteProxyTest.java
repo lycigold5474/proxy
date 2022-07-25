0bcf1e0e406e2b69590a73dfd4cc18dde0f337c4
andy <lycigold@gmail.com>
2022-07-25 10:05:11 +0900

    구체기클래스기반 프록시 예제2 -TimeProxy적용

--- src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pureproxy.concreteproxy;
 
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteClient;
 import hello.proxy.pureproxy.concreteproxy.code.ConcreteLogic;
+import hello.proxy.pureproxy.concreteproxy.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteProxyTest {
@@ -12,4 +13,16 @@ public class ConcreteProxyTest {
         ConcreteClient client = new ConcreteClient(concreteLogic);
         client.execute();
     }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        //TimeProxy를 추가하자 TimeProxy는 생성자에 ConcreteLogic이 필요함
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        //client는 proxy를 호출하자
+        ConcreteClient client = new ConcreteClient(timeProxy);
+        client.execute();
+    }
+
+
 }
---
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/code/TimeProxy.java
@@ -0,0 +1,28 @@
+package hello.proxy.pureproxy.concreteproxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeProxy extends ConcreteLogic {
+    private ConcreteLogic concreteLogic;
+
+    public TimeProxy(ConcreteLogic concreteLogic) {
+        this.concreteLogic = concreteLogic;
+    }
+
+    // 오버라이드하여 다형성을 적용하자.
+    // 시간을 측정하는 부가 기능을 추가
+    @Override
+    public String operation() {
+        log.info("TimeDecorator 실행");
+        long startTime = System.currentTimeMillis();
+
+        String result = concreteLogic.operation();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeDecorator 종료 reusltTime={}ms", resultTime);
+        return result;
+
+    }
+}
